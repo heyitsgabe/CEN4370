7a951880f791cb2ba07ab2e75984688c35a87f28
Gabriel Morales <heyitsgabe@users.noreply.github.com>
2017-08-02 00:58:33 -0400

    MapsFinal
    Initial upload of maps application build with MonoDevelop. MonoDevelop was used for syntax checking, must run in visual studio for testing. Should launch new Google Maps page in ASP.NET form.

---
+++ MapsFinal/MapsFinal/App_Start/MapsSource.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using DotNetBrowser;
+using DotNetBrowser.WinForms;
+using System;
+using System.Windows.Forms;
+
+namespace MapsFinal
+{
+    public partial class MapsForm: MapsForm {
+        private BrowserView browserview;
+
+        public MainForm() {
+            InitializeComponent();
+            browserview = new WinFormsBrowserView();
+            this.controls.Add((control)browserview.GetComponent());
+            browserview.Browser.LoadURL("http://maps.google.com");
+        }
+    }
+}
